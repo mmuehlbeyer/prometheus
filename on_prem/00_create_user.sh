4207049b5f8d5e928eb96171ca96a11abedb9286
mmuehlbeyer <m.muehlbeyer@gmx.de>
2021-01-11 16:44:18 +0100

    added user script

---
+++ on_prem/00_create_user.sh
@@ -0,0 +1,5 @@
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo useradd --no-create-home --shell /bin/false alertmanager
+sudo useradd --no-create-home --shell /bin/false blackbox
+
+
